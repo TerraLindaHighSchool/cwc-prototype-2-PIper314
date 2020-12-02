30993803eb86787aeddb025ef9ab3ce7674d7eab
PIper314 <pe01790@srcs.org>
2020-12-02 14:56:40 -0800

    tutorial 2.1.3 -2.1.6 complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizonatalInput;
+    public float speed = 10.0f
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizonatalInput = horizonatalInput.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizonatalInput * Time.deltaTime * speed);
     }
 }
